adf100d033aa14a04f45b70846b6366f49307724
vincent <hkvincentchan@gmail.com>
2023-06-25 23:42:56 +0800

    refactor the root layot and components

---
+++ app/components/Header.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const Header = () => {
+    return (
+        <div>
+            {/* HEADER */}
+            <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+                <div className="text-center mt-10">
+                    <h1 className="text-white text-5xl font-bold mb-2">
+                        Find your table for any occasion
+                    </h1>
+                    {/* SEARCH BAR */}
+                    <SearchBar />
+                    {/* SEARCH BAR */}
+                </div>
+            </div>
+            {/* HEADER */}
+        </div>
+    );
+};
+
+export default Header;
