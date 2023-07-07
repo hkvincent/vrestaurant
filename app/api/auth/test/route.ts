9c325fe89983f31ce6152b30db8d8061dc5df462
vincent <hkvincentchan@gmail.com>
2023-07-08 00:00:29 +0800

    middleware added

--- app/api/auth/test/route.ts
+++ app/api/auth/test/route.ts
@@ -1,10 +1,24 @@
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
-export async function GET(req: Request) {
-    const body = await req.json();
+const prisma = new PrismaClient();
 
-    const bearerToken = body.headers["authorization"] as string;
+export async function GET(req: Request) {
+    const headers = req.headers;
+    console.log({ headers });
+
+    const bearerToken = headers.get("authorization") as string;
+
+    if (!bearerToken) {
+        return new Response(JSON.stringify("Unauthorized request"),
+            {
+                headers: { 'Content-Type': 'application/json' },
+                status: 401,
+            });
+    }
+
+    console.log({ bearerToken });
+
     const token = bearerToken.split(" ")[1];
 
     const payload = jwt.decode(token) as { email: string };
@@ -17,11 +31,34 @@ export async function GET(req: Request) {
             });
     }
 
+    const user = await prisma.user.findUnique({
+        where: {
+            email: payload.email,
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            city: true,
+            phone: true,
+        },
+    });
 
+    console.log({ user });
 
-    return new Response(JSON.stringify("Username or Password is incorrect"),
+    if (!user) {
+        return new Response(JSON.stringify("Unauthorized request"),
+            {
+                headers: { 'Content-Type': 'application/json' },
+                status: 401,
+            });
+    }
+
+    return new Response(JSON.stringify(user),
         {
             headers: { 'Content-Type': 'application/json' },
-            status: 401,
+            status: 200,
         });
+
 }
---
+++ middleware.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import * as jose from "jose";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const bearerToken = request.headers.get("authorization") as string;
+
+    if (!bearerToken) {
+        return new NextResponse(
+            JSON.stringify({ errorMessage: "Unauthorized request" }),
+            { status: 401 }
+        );
+    }
+
+    const token = bearerToken.split(" ")[1];
+
+    if (!token) {
+        return new NextResponse(
+            JSON.stringify({ errorMessage: "Unauthorized request" }),
+            { status: 401 }
+        );
+    }
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+    try {
+        await jose.jwtVerify(token, secret);
+    } catch (error) {
+        return new NextResponse(
+            JSON.stringify({ errorMessage: "Unauthorized request" }),
+            { status: 401 }
+        );
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+}
