615fa851b6aba0a55d75ccb8e22247b5879ca1d7
vincent <hkvincentchan@gmail.com>
2023-08-14 19:05:27 +0800

    build error fix

--- app/action.ts
+++ app/action.ts
@@ -1,71 +1,72 @@
-'use server';
-import validator from "validator";
-import bcrypt from "bcrypt";
+export const register = "register";
+// 'use server';
+// import validator from "validator";
+// import bcrypt from "bcrypt";
 
-export const register = withValidate((data: any) => {
-    // ...
-})
-
-
-export function withValidate(action: any) {
-    return async (formData: FormData) => {
-        'use server'
-
-        const isValidData: string | boolean = verifyData(formData)
-
-        if (!isValidData) {
-            throw new Error(isValidData as string)
-        }
-
-        //const data = process(formData)
-        return action(data)
-    }
-}
+// export const register = withValidate((data: any) => {
+//     // ...
+// })
 
 
-export function verifyData(formData: FormData) {
-    const { firstName, lastName, email, phone, city, password } = formData;
-    const errors: string[] = [];
+// export function withValidate(action: any) {
+//     return async (formData: FormData) => {
+//         'use server'
+
+//         const isValidData: string | boolean = verifyData(formData)
+
+//         if (!isValidData) {
+//             throw new Error(isValidData as string)
+//         }
+
+//         //const data = process(formData)
+//         return action(data)
+//     }
+// }
 
 
-    const validationSchema = [
-        {
-            valid: validator.isLength(firstName, {
-                min: 1,
-                max: 20,
-            }),
-            errorMessage: "First name is invalid",
-        },
-        {
-            valid: validator.isLength(lastName, {
-                min: 1,
-                max: 20,
-            }),
-            errorMessage: "First name is invalid",
-        },
-        {
-            valid: validator.isEmail(email),
-            errorMessage: "Email is invalid",
-        },
-        {
-            valid: validator.isMobilePhone(phone),
-            errorMessage: "Phone number is invalid",
-        },
-        {
-            valid: validator.isLength(city, { min: 1 }),
-            errorMessage: "City is invalid",
-        },
-        {
-            valid: validator.isStrongPassword(password),
-            errorMessage: "Password is not strong enough",
-        },
-    ];
+// export function verifyData(formData: FormData) {
+//     const { firstName, lastName, email, phone, city, password } = formData;
+//     const errors: string[] = [];
 
-    validationSchema.forEach((check) => {
-        if (!check.valid) {
-            errors.push(check.errorMessage);
-        }
-    });
 
-    return errors.length === 0 ? errors[0] : false
-}
+//     const validationSchema = [
+//         {
+//             valid: validator.isLength(firstName, {
+//                 min: 1,
+//                 max: 20,
+//             }),
+//             errorMessage: "First name is invalid",
+//         },
+//         {
+//             valid: validator.isLength(lastName, {
+//                 min: 1,
+//                 max: 20,
+//             }),
+//             errorMessage: "First name is invalid",
+//         },
+//         {
+//             valid: validator.isEmail(email),
+//             errorMessage: "Email is invalid",
+//         },
+//         {
+//             valid: validator.isMobilePhone(phone),
+//             errorMessage: "Phone number is invalid",
+//         },
+//         {
+//             valid: validator.isLength(city, { min: 1 }),
+//             errorMessage: "City is invalid",
+//         },
+//         {
+//             valid: validator.isStrongPassword(password),
+//             errorMessage: "Password is not strong enough",
+//         },
+//     ];
+
+//     validationSchema.forEach((check) => {
+//         if (!check.valid) {
+//             errors.push(check.errorMessage);
+//         }
+//     });
+
+//     return errors.length === 0 ? errors[0] : false
+// }
